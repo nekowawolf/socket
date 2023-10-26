8095d7b3b3fd268aad7ec533597b399cf3e34faa
nekowawolf <irukainu20@gmail.com>
2023-10-26 13:43:51 -0700

    ip address

---
+++ Latihan5.py
@@ -0,0 +1,12 @@
+import socket
+def convert_integer():
+    data = 1234
+    #32-bit
+    print ("original: %s ==> Long Host Byte Order: %s, Network byte order: %s"
+           %(data, socket.ntohl(data), socket.htonl(data)))
+    #16-bit
+    print ("original: %s ==> Short Host Byte Order: %s, Network byte order: %s"
+           %(data, socket.ntohl(data), socket.htonl(data)))
+    
+    if __name__ == '__main__':
+        convert_integer()
